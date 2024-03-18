5a83f9d3aae8116a24667b2e6551b28ed66c4b44
Manu Arya <manuddn15@gmail.com>
2024-03-19 00:39:05 +0530

    Added build files

---
+++ precache-manifest.5ebe19cee4d0d271d53fba10d01b828f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab628026fae5f726e0a710704189556",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a1811f78f9af4ca42ce2",
+    "url": "/static/css/main.990e71b9.chunk.css"
+  },
+  {
+    "revision": "2fbe55b1e1e439f69dda",
+    "url": "/static/js/2.6d5c1e93.chunk.js"
+  },
+  {
+    "revision": "a1811f78f9af4ca42ce2",
+    "url": "/static/js/main.940f8857.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e10590f340b9ee8eb971585e968dfddc",
+    "url": "/static/media/Brier-Bold.e10590f3.otf"
+  },
+  {
+    "revision": "fb133bf8b2bca70ccd226d59292d0dda",
+    "url": "/static/media/Brier-Regular.fb133bf8.otf"
+  },
+  {
+    "revision": "d67f774ee406fbdf511d4cffaa258e97",
+    "url": "/static/media/DetailsPageImg.d67f774e.jpeg"
+  },
+  {
+    "revision": "3b32d12939fc7a66e3736c12e3f2dfae",
+    "url": "/static/media/ErrorPageImg.3b32d129.jpeg"
+  },
+  {
+    "revision": "37820b1329a9658746330cf8064a95de",
+    "url": "/static/media/HomePageImg.37820b13.jpeg"
+  },
+  {
+    "revision": "3f619550ff38cc63e8f38ccb431ef8e3",
+    "url": "/static/media/searchPageImg.3f619550.jpeg"
+  }
+]);
